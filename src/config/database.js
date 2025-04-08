51b061a0ecc42c8a6c9db2bb3885b5f7202e8777
Bui Cong Hieu <buihieu9259@gmail.com>
2025-04-08 17:19:31 +0700

    video 58 + 59

--- src/config/database.js
+++ src/config/database.js
@@ -1,34 +1,37 @@
 
 require('dotenv').config();// phải import cái này vì nếu ko imporrt vào thì nó ko lấy đc dữ liệu của env (t nghĩ thếnst mysql = require('mysql2');
 const mongoose = require('mongoose');
-var dbState = [{
+const dbState = [{
     value: 0,
-    label: "disconnected"
+    label: "Disconnected"
 },
 {
     value: 1,
-    label: "connected"
+    label: "Connected"
 },
 {
     value: 2,
-    label: "connecting"
+    label: "Connecting"
 },
 {
     value: 3,
-    label: "disconnecting"
+    label: "Disconnecting"
 }];
 
-const connection = async () =>{
+const connection = async () => {
 
-    // Or:
     try {
-        
-        await mongoose.connect('mongodb://root:123456@localhost:27018');
+        const options = {
+            user: process.env.DB_USER,
+            pass: process.env.DB_PASSWORD,
+            dbName: process.env.DB_NAME
+        }
+        await mongoose.connect(process.env.DB_HOST, options);
         const state = Number(mongoose.connection.readyState);
-        console.log(dbState.find(f => f.value == state).label, "to db"); // connected to db
+        console.log(dbState.find(f => f.value == state).label, "to database"); // connected to db
     } catch (error) {
         // handleError(error);
         console.log('Error connecting to MongoDB', error);
-      }
+    }
 }
 module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web')
 const connection = require('./config/database')
 const { table } = require('console');
+const mongoose = require('mongoose')
+
 
 // import express from 'express'; //es module
 const app = express()// app express
@@ -15,20 +17,35 @@ const hostname = process.env.HOST_NAME;
 // cái này có tác dụng duy nhất hỗ trọ lấy input từ html rồi gửi lên server
 // config req.body
 app.use(express.json( )); // Used to parse JSON bodies
-app.use(express.urlencoded( )); //Parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
 
 // config template engine
 configViewEngine(app);
 
 // khai báo route
-app.use('/',webRoutes)
+app.use('/',webRoutes);
 
 // test connection
 connection();
 
-//run server trên port đã khởi tạo trước đấy
-//nạp các thông tin khai báo ở trên rồi chạy (ví dụ như nạp routes)
-app.listen(port, hostname, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const kittySchema = new mongoose.Schema({
+  name: String
+});
+
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+const cat = new Kitten({ name: 'hoi coin card' });
+
+cat.save();
+
+//SELF RUNNING FUNCTION
+;(async( ) => {
+  try{
+    await connection();
+    app.listen(port, hostname, () => {
+      console.log(`Example app listening on port ${port}`)
+    })
+  }catch(error){
+    console.log(">> error connect to db",error);}
+  }) ( )
 
