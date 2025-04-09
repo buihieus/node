c21c354df448156cbcc1ad887fc77ff99a874257
Bui Cong Hieu <buihieu9259@gmail.com>
2025-04-09 21:57:46 +0700

    video 76 update a user

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -36,7 +36,37 @@ const postCreateUserAPI = async (req, res) => {
     );
 
 }
+const postUpdateUserAPI = async (req, res) => {
+
+    // lấy biến email,myname,city từ html
+    let email = req.body.email;
+    let name = req.body.myname;
+    let city = req.body.city;
+    let userId = req.body.userId;
+
+
+    // await updateUsersById(email, name, city, userId);
+    let user = await User.updateOne({ _id: userId }, { name: name, email: email, city: city });
+    return res.status(200).json(
+        {
+            errorCode: 0,
+            data: user
+        }
+    );// nhờ ré nỳ thì đường link nó mớ trở thành api
+}
+const postDeleteUserAPI = async (req, res) => {
+    const userId = req.params.id;
+    // let user = await getUsersById(userId);
+    let user = await User.findById(userId).exec();
+    return res.status(200).json(
+        {
+            errorCode: 0,
+            data: user
+        }
+    );
+}
 module.exports = {
     getUsersAPI,
-    postCreateUserAPI
+    postCreateUserAPI,
+    postUpdateUserAPI,
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const routerAPI = express.Router();
-const {getUsersAPI,postCreateUserAPI} = require('../controllers/apiController')
+const {getUsersAPI,postCreateUserAPI,postUpdateUserAPI} = require('../controllers/apiController')
 //nơi khai báo và thông báo cho express là sẽ gọi cái nào
 
 
 routerAPI.post('/users',postCreateUserAPI);
 
 routerAPI.get('/users',getUsersAPI);
+
+routerAPI.put('/users',postUpdateUserAPI);
+
 module.exports = routerAPI;// export default 
