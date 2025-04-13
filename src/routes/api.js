989b00a4b6bb2dc47a9131efda7ea57ee49cefde
Bui Cong Hieu <buihieu9259@gmail.com>
2025-04-13 23:19:18 +0700

    video 94

--- src/routes/api.js
+++ src/routes/api.js
@@ -29,4 +29,10 @@ routerAPI.put('/customers',putUpdateCustomers);
 routerAPI.delete('/customers',deleteACustomer);
 routerAPI.delete('/customers-many',deleteArrayCustomer);
 
+routerAPI.get('/info',(req,res)=>{
+    console.log(">> check req.query",req.query);
+    return res.status(200).json({
+    data: req.query
+    });
+});
 module.exports = routerAPI;// export default 
